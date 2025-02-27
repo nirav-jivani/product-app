001210321f3ea87d4213f67099ffe962c733429e
nirav-jivani <niravjivani82@gmail.com>
2025-02-26 23:52:37 -0500

    Product Details Component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-// import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: 'products', component: ProductListComponent },
-    // { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
     { path: '', redirectTo: '/products', pathMatch: 'full' }
   ];
   @NgModule({
---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product = signal<Product | undefined>(undefined);
+  router = inject(Router);
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProductById(id).subscribe(data => {
+      this.product.set(data);
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/products']);
+    }
+
+}
